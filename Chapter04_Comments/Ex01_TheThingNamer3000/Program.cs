da7806a1b3f2d3b741938ec8c5cb03d9c90cf8ab
ersmax <masarin.eros@gmail.com>
2026-05-28 09:56:04 +0200

    add ex

---
+++ Chapter04_Comments/Ex01_TheThingNamer3000/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+
+// A thing
+string thing = Console.ReadLine();  
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+// An adjective
+string adjective = Console.ReadLine();  
+string whom = "Doom";
+string number = "3000";
+Console.WriteLine("The " + adjective + " " + thing + " of " + whom + " " + number + "!");
