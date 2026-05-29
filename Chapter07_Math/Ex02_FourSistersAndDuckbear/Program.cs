78ebf5574a54f9fd0cc0bb9cc5572bf5813bc2d6
ersmax <masarin.eros@gmail.com>
2026-05-29 10:48:39 +0200

    Add sisters and duckbear

---
+++ Chapter07_Math/Ex02_FourSistersAndDuckbear/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter the number of chocolate eggs picked up today: ");
+ushort eggs = Convert.ToUInt16(Console.ReadLine());
+
+int sisterEggs = eggs / 4;
+int duckbearEggs = eggs % 4;
+
+Console.WriteLine("Each sister gets " + sisterEggs + " eggs.");
+Console.WriteLine("Duckbear gets " + duckbearEggs + " eggs.");
+
+// If we had 3 eggs, sisters would get no eggs and the duckbear all 3.
+// If we had 7 eggs, sisters would get 1 egg each and the duckbear 3.
+// If we had 11 eggs, sisters would get 2 eggs each and the duckbear 3.
+// At 15 eggs, they would get equal amount of eggs.
+// Beyond that, sisters would have more eggs than duckbear.
+// A solution could be a loop, to count how many more eggs duckbear would have than sisters.
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -4,3 +4,7 @@ double avogadrosNumber = 6.022e23;
 Console.WriteLine(int.MaxValue);
 Console.WriteLine(double.MinValue);
 Console.WriteLine(double.PositiveInfinity);
+Console.WriteLine(double.NegativeInfinity);
+Console.WriteLine(double.NaN);
+// NaN because 0 is interpreted as small quantity for double division
+Console.WriteLine(avogadrosNumber / 0); 
