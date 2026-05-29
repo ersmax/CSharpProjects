61e81e8f446e733c73360272076301e91346ac12
ersmax <masarin.eros@gmail.com>
2026-05-29 15:08:42 +0200

    add The Dominion of Kings

---
+++ Chapter07_Math/Ex03_DominionOfKings/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Melik's kingdom");
+Console.Write("Enter number of estates: ");
+uint estates = Convert.ToUInt32(Console.ReadLine());
+Console.Write("Enter number of duchies: ");
+uint duchies = Convert.ToUInt32(Console.ReadLine());
+Console.Write("Enter number of provinces: ");
+uint provinces = Convert.ToUInt32(Console.ReadLine());
+
+uint points = estates * 1 + duchies * 3 + provinces * 6;
+Console.WriteLine("Tot points: " + points);
+
