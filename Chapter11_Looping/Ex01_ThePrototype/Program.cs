a309881605ac3270d32a770ab5ed780a61f94e7d
ersmax <masarin.eros@gmail.com>
2026-05-31 21:02:23 +0200

    Add the prototype

---
+++ Chapter11_Looping/Ex01_ThePrototype/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Title = "The Prototype";
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+int target;
+do
+{
+    Console.Write("User 1, enter a number between 0 and 100: ");
+    target = Convert.ToInt32(Console.ReadLine());
+} while (target < 0 || target > 100);
+
+Console.Clear();
+Console.WriteLine("User 2, guess the number");
+
+bool found = false;
+while (!found)
+{
+    Console.Write("What is your next guess? ");
+    int guess = Convert.ToInt32(Console.ReadLine());
+    if (guess == target)
+        found = true;
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write($"{guess} is too ");
+        if (guess < target)
+            Console.WriteLine("low.");
+        else
+            Console.WriteLine("high.");
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("You guessed the number!");
+
+
+
+ 
