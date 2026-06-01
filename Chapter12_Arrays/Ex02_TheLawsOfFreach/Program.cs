94f8e9968b6391fae1bf83a3068c49198e6ecc3c
ersmax <masarin.eros@gmail.com>
2026-06-01 11:25:35 +0200

    add laws

---
+++ Chapter12_Arrays/Ex02_TheLawsOfFreach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+Console.Title = "The Laws of Freach";
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int minimum = int.MaxValue;
+float total = 0f;
+for (int idx = 0; idx <  array.Length; idx++)
+{
+    minimum = (array[idx] <  minimum) ? array[idx] : minimum;
+    total += array[idx];
+}
+
+double average = total / array.Length;
+Console.WriteLine($"Minimum: {minimum, 10}");
+Console.WriteLine($"Average: {average, 10:F2}");
+
+// Foreach part
+minimum = int.MaxValue;
+total = 0f;
+foreach (int number in array)
+{
+    minimum = (number < minimum) ? number : minimum;
+    total += number;
+}
+average = total / array.Length;
+Console.WriteLine($"Minimum: {minimum,10}");
+Console.WriteLine($"Average: {average,10:F}");
---
+++ Chapter12_Arrays/Ex02_TheLawsOfFreach/README.md
@@ -0,0 +1,13 @@
+The Laws of Freach
+-
+The town of Freach recently had an awful looping disaster. The lead investigator found that it was a faulty
+++ operator in an old for loop, but the town council has chosen to ban all loops but the foreach loop.
+Yet Freach uses the code presented earlier in this level to compute the minimum and the average value
+in an int array. They have hired you to rework their existing for-based code to use foreach loops
+instead.
+
+---
+#### Objectives:
+- Start with the code for computing an array’s minimum and average values in the section Some Examples with Arrays, starting on page 94.
+- Modify the code to use foreach loops instead of for loops.
+
