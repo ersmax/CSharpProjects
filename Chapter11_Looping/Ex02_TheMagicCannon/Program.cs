a5458419046476af3c6ab831c0d05b91687acf8c
ersmax <masarin.eros@gmail.com>
2026-06-01 08:45:34 +0200

    add the magic cannon

---
+++ Chapter11_Looping/Ex02_TheMagicCannon/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Title = "The Magic Cannon";
+
+for (int idx = 1; idx <= 100; idx++)
+{
+    string blast = "Normal";
+
+    if (idx % 3 == 0)
+        if (idx % 5 == 0)
+        {
+            blast = "Combined";
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        else
+        {
+            blast = "Fire";
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+    else if (idx % 5 == 0)
+    {
+        blast = "Electric";
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    }
+
+    Console.WriteLine($"{idx,-3}: {blast}");
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
