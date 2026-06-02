dce096625e162b0715635ed17414f11fbb34166a
ersmax <masarin.eros@gmail.com>
2026-06-02 15:28:24 +0200

    add countdown

---
+++ Chapter13_Methods/Ex02_Countdown/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Title = "Countdown";
+
+CountDown(10);
+
+/// <summary>
+/// Counts down from 10 to 1
+/// </summary>
+void CountDown(int number)
+{
+    if (number < 1)
+        return;
+    Console.WriteLine(number);
+    CountDown(number - 1);
+}
