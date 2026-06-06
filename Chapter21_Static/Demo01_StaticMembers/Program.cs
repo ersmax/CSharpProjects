48b4e3914af02194ee74ce3c4f446d68a15438e1
ersmax <masarin.eros@gmail.com>
2026-06-06 17:40:21 +0200

    add demo static members

---
+++ Chapter21_Static/Demo01_StaticMembers/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Score
+{
+    private static readonly int PointsThreshold = 1000;
+    private static readonly int LevelThreshold = 4;
+    public static int PropertyPointsThreshold { get; } = 1000;
+    public static int PropertyLevelThreshold { get; } = 4;
+    public int Points { get; set; }
+    public int Level { get; set; }
+    public Score() : this(0, 0) { }
+    public Score(int points, int level)
+    {
+        Points = points;
+        Level = level;
+    }
+    
+    public bool IsWorthyOfTheHighScoreTable()
+    {
+        if (Points < PointsThreshold) return false;
+        if (Level < LevelThreshold) return false;
+        return true;
+    }
+
+    public bool IsWorthyProperty()
+    {
+        if (Points < PropertyPointsThreshold) return false;
+        if (Level < PropertyLevelThreshold) return false;
+        return true;
+    }
+}
