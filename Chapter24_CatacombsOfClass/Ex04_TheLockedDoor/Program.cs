4ce2351b09420d0a598a31e34ca05ff3383539b3
ersmax <masarin.eros@gmail.com>
2026-06-08 21:31:32 +0200

    refactor code

--- Chapter24_CatacombsOfClass/Ex04_TheLockedDoor/Program.cs
+++ Chapter24_CatacombsOfClass/Ex04_TheLockedDoor/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.Title = "The Locked Door";
 
-Door aDoor = new Door(ChooseInitialPwd(), ChooseStatus());
+Door aDoor = new Door(EnterNumber("Enter passcode:"), ChooseStatus());
 Console.Clear();
 while (true)
 {
     handleChoice(aDoor);
     Console.Write("Exit (y/n)? ");
-    if (Console.ReadLine().ToLower() == "y")
+    if (Console.ReadLine()?.ToLower() == "y")
         break;
 }
 
@@ -16,39 +16,50 @@ Status ChooseStatus()
     string display = "";
     foreach (string option in options)
         display += $" \"{option}\"";
-    Console.WriteLine($"What option do you choose among these? {display}");
-    Status input = Enum.Parse<Status>(Console.ReadLine(), true);
-    return input;
-}
 
-string ChooseInitialPwd()
-{
-    Console.Write("Enter password: ");
-    return Console.ReadLine();
+    bool valid = false;
+    string? inputString;
+    do
+    {
+        valid = false;
+        Console.WriteLine($"What option do you choose among these? {display}");
+        inputString = Console.ReadLine();
+        foreach (string option in options)
+            if (inputString?.ToLower() == option.ToLower())
+            {
+                valid = true;
+                break;
+            }
+      
+    } while (!valid || inputString == null);
+
+    Status input = Enum.Parse<Status>(inputString, true);
+    return input;
 }
 
 void handleChoice(Door aDoor)
 {
     Console.WriteLine($"Door status: {aDoor.Status}.");
-    Console.WriteLine("Enter action among these ('open', 'close', 'locking', 'unlocking', 'changing passcode')");
+    Console.WriteLine("Enter action among these ('Open', 'Close', 'Lock', 'Unlock', 'Changing passcode')");
     Console.Write("> ");
-    string input = Console.ReadLine();
+    string? input = Console.ReadLine()?.ToLower();
     switch (input)
     {
         case "open":
-            aDoor.open();
+            aDoor.Open();
             break;
         case "close":
-            aDoor.close();
+            aDoor.Close();
             break;
-        case "locking":
-            aDoor.locking();
+        case "lock":
+            aDoor.Lock();
             break;
-        case "unlocking":
-            aDoor.unlocking();
+        case "unlock":
+            aDoor.Unlock();
             break;
         case "changing passcode":
-            aDoor.changePassword();
+            int oldPasscode = EnterNumber("Enter old passcode:");
+            aDoor.ChangePasscode();
             break;
         default:
             Console.WriteLine("Not a valid option");
@@ -56,62 +67,61 @@ void handleChoice(Door aDoor)
     }
 }
 
+int EnterNumber(string text)
+{
+    Console.Write(text + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
 public class Door
 {
-    private string Passcode { get; set; }
+    private int _passcode;
     public Status Status { get; private set; }
 
-    public Door() : this("password", Status.Open) { }
+    public Door() : this(1234, Status.Open) { }
 
-    public Door(string password) : this(password, Status.Open) { }
+    public Door(int passcode) : this(passcode, Status.Open) { }
 
-    public Door(string password, Status status)
+    public Door(int passcode, Status status)
     {
-        Passcode = password;
+        _passcode = passcode;
         Status = status;
     }
 
-    public void close()
+    public void Close()
     {
         if (Status == Status.Open)
             Status = Status.Closed;
     }
-    public void open()
+    public void Open()
     {
         if (Status == Status.Closed)
             Status = Status.Open;
     }
-    public void locking() 
+    public void Lock() 
     {
         if (Status == Status.Closed)
             Status = Status.Locked;
     }
-    public void unlocking()
+    public void Unlock()
     {
         Console.Write("Enter password: ");
-        string password = Console.ReadLine();
-        if (Status == Status.Locked && password == Passcode)
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (Status == Status.Locked && input == _passcode)
             Status = Status.Closed;
     }
 
-    public void changePassword()
+    public void ChangePasscode()
     {
-        Console.Write("Enter the old password: ");
-        string input = Console.ReadLine();
-        if (input != Passcode)
+        Console.Write("Enter the old passcode: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input != _passcode)
         {
-            Console.WriteLine("Password do not match. Abort");
+            Console.WriteLine("Passcode does not match. Abort");
             return;
         }
-        while (true)
-        { 
-            Console.Write("Enter new password: ");
-            input = Console.ReadLine();
-            if (input == null)
-                continue;
-            else break;
-        }
-        Passcode = input;
+        Console.Write("Enter new password: ");
+        input = Convert.ToInt32(Console.ReadLine());
+        _passcode = input;
     }
 
 }
